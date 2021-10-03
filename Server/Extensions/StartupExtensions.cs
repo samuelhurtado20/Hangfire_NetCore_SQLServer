c3fc7ea9e4a6deb10ad19c809d5468248e0a0999
samuelhurtado20 <samuelhurtado20@hotmail.com>
2021-10-02 22:04:28 -0500

    first basic example

---
+++ Server/Extensions/StartupExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hangfire_NetCore_SQLServer.Server.Extensions
+{
+	public static class StartupExtensions
+	{
+		public static void ConfigHangfire(this IServiceCollection services, IConfiguration configuration)
+		{
+			// Add Hangfire services.
+			services.AddHangfire(config => config
+				.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+				.UseSimpleAssemblyNameTypeSerializer()
+				.UseRecommendedSerializerSettings()
+				.UseSqlServerStorage(configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+				{
+					CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+					SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+					QueuePollInterval = TimeSpan.Zero,
+					UseRecommendedIsolationLevel = true,
+					DisableGlobalLocks = true
+				}));
+
+			// Add the processing server as IHostedService
+			services.AddHangfireServer();
+
+		}
+	}
+}
